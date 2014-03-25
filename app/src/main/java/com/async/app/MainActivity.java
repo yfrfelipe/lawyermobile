5dda74db85a23217e7567253f8dc2776dbb1fbff
Yrineu Rodrigues <yfrfelipe@gmail.com>
2014-03-25 10:24:44 -0300

    Testing change on .java

--- app/src/main/java/com/async/app/MainActivity.java
+++ app/src/main/java/com/async/app/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
+            
         }
     }
 
